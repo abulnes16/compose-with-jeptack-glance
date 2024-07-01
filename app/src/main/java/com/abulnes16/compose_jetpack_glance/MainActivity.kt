ab6361b330d5983af98e30509b32981132b18cd8
Angel Bulnes <abulnes16@MacBook-Pro-de-Angel.local>
2024-06-30 22:31:15 -0600

    Initial project files

---
+++ app/src/main/java/com/abulnes16/compose_jetpack_glance/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.abulnes16.compose_jetpack_glance
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.abulnes16.compose_jetpack_glance.ui.theme.Compose_Jetpack_GlanceTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Compose_Jetpack_GlanceTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Compose_Jetpack_GlanceTheme {
+        Greeting("Android")
+    }
+}
